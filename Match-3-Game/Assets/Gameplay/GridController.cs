41d9e3087d7e14a491cf9ee4d7a10fb57d06fa6f
frankvega2010 <franco_vega@msn.com>
2019-09-09 09:44:36 -0300

    Moved functions from GridModel to GridController

--- Match-3-Game/Assets/Gameplay/GridController.cs
+++ Match-3-Game/Assets/Gameplay/GridController.cs
@@ -11,14 +11,15 @@ public class GridController : MonoBehaviour
     void Start()
     {
         grid = new GridModel();
-        grid.InitializeGrid(9,9,2);
-        grid.ShowGridData();
+        InitializeGrid(9,9,2);
+        ShowGridData();
 
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
             {
-                gridView.DrawGrid(grid.GetColor(i,j),grid.GetPosition(i,j));
+                //gridView.DrawGrid(grid.GetColor(i,j),grid.GetPosition(i,j));
+                gridView.DrawGrid(grid.gridColors[i,j], grid.gridPositions[i,j]);
                 //gridColors[i, j] = (Colors)Random.Range(1, 4);
                 //gridPositions[i, j] = new Vector2(increasedPosition * i, increasedPosition * j);
             }
@@ -30,4 +31,110 @@ public class GridController : MonoBehaviour
     {
         
     }*/
+
+    public void InitializeGrid(int newHeight, int newWidth, int increasedPosition)
+    {
+        grid.cellPosition = 0;
+        grid.width = newWidth;
+        grid.height = newHeight;
+
+        grid.gridColors = new GridModel.Colors[grid.height, grid.width];
+        grid.gridPositions = new Vector2[grid.height, grid.width];
+
+        for (int i = 0; i < grid.height; i++)
+        {
+            for (int j = 0; j < grid.width; j++)
+            {
+                grid.gridColors[i, j] = RandomColor();
+                grid.gridPositions[i, j] = new Vector2(increasedPosition * j, increasedPosition * i);
+            }
+        }
+
+        CheckRepeatedColorsVertical();
+        CheckRepeatedColorsHorizontal();
+    }
+
+    public void CheckRepeatedColorsVertical()
+    {
+        int findSameColor = 0;
+
+        for (int i = 0; i < grid.height; i++)
+        {
+            for (int j = 0; j < grid.width; j++)
+            {
+                if (i != 0 && i < grid.height - 1)
+                {
+                    findSameColor++;
+
+                    if (grid.gridColors[i, j] == grid.gridColors[i - 1, j])
+                    {
+                        findSameColor++;
+                    }
+
+                    if (grid.gridColors[i, j] == grid.gridColors[i + 1, j])
+                    {
+                        findSameColor++;
+                    }
+
+                    if (findSameColor >= 3)
+                    {
+                        //grid.gridColors[i, j] = GridModel.Colors.blank;
+                        grid.gridColors[i, j] = RandomColor();
+                    }
+
+                    findSameColor = 0;
+                }
+            }
+        }
+    }
+
+    public void CheckRepeatedColorsHorizontal()
+    {
+        int findSameColor = 0;
+
+        for (int i = 0; i < grid.height; i++)
+        {
+            for (int j = 0; j < grid.width; j++)
+            {
+                if (j != 0 && j < grid.width - 1)
+                {
+                    findSameColor++;
+
+                    if (grid.gridColors[i, j] == grid.gridColors[i, j - 1])
+                    {
+                        findSameColor++;
+                    }
+
+                    if (grid.gridColors[i, j] == grid.gridColors[i, j + 1])
+                    {
+                        findSameColor++;
+                    }
+
+                    if (findSameColor >= 3)
+                    {
+                        //grid.gridColors[i, j] = GridModel.Colors.blank;
+                        grid.gridColors[i, j] = RandomColor();
+                    }
+
+                    findSameColor = 0;
+                }
+            }
+        }
+    }
+
+    public void ShowGridData()
+    {
+        for (int i = 0; i < grid.height; i++)
+        {
+            for (int j = 0; j < grid.width; j++)
+            {
+                Debug.Log("Position " + i + " " + j + " = " + "Color : " + grid.gridColors[i, j] + " / Position in Matrix = " + grid.gridPositions[i, j]);
+            }
+        }
+    }
+
+    private GridModel.Colors RandomColor()
+    {
+        return (GridModel.Colors)Random.Range(1, 4);
+    }
 }
--- Match-3-Game/Assets/Gameplay/GridModel.cs
+++ Match-3-Game/Assets/Gameplay/GridModel.cs
@@ -13,45 +13,13 @@ public class GridModel
         allColors
     }
 
-    private Colors[,] gridColors;
-    private Vector2[,] gridPositions;
-    private int cellPosition;
-    private int width;
-    private int height;
+    public Colors[,] gridColors;
+    public Vector2[,] gridPositions;
+    public int cellPosition;
+    public int width;
+    public int height;
 
-    public void InitializeGrid(int newHeight,int newWidth,int increasedPosition)
-    {
-        cellPosition = 0;
-        width = newWidth;
-        height = newHeight;
-
-        gridColors = new Colors[height, width];
-        gridPositions = new Vector2[height, width];
-
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                gridColors[i,j] = (Colors)Random.Range(1, 4);
-                gridPositions[i, j] = new Vector2(increasedPosition * j, increasedPosition * i);
-            }
-        }
-
-        CheckRepeatedColorsVertical();
-    }
-
-    public void ShowGridData()
-    {
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                Debug.Log("Position " + i + " " + j + " = " + "Color : " + gridColors[i, j] + " / Position in Matrix = " + gridPositions[i, j]);
-            }
-        }
-    }
-
-    public Vector2 GetPosition(int column, int row)
+   /* public Vector2 GetPosition(int column, int row)
     {
         Vector2 position;
 
@@ -67,38 +35,7 @@ public class GridModel
         color = gridColors[column, row];
 
         return color;
-    }
+    }*/
 
-    public void CheckRepeatedColorsVertical()
-    {
-        int findSameColor = 0;
-
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                if(i != 0 && i < height - 1)
-                {
-                    findSameColor++;
-
-                    if (gridColors[i, j] == gridColors[i-1, j])
-                    {
-                        findSameColor++;
-                    }
-
-                    if (gridColors[i, j] == gridColors[i + 1, j])
-                    {
-                        findSameColor++;
-                    }
-
-                    if(findSameColor >= 3)
-                    {
-                        gridColors[i, j] = Colors.blank;
-                    }
-
-                    findSameColor = 0;
-                }
-            }
-        }
-    }
+    
 }
